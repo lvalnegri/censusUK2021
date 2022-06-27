899472a97f89ebb1a6a65236e1bb1dc5c948bd03
datamaps <l.valnegri@datamaps.co.uk>
2022-06-27 01:40:24 +0100

    first commit

---
+++ R/census_uk_2021.R
@@ -0,0 +1,9 @@
+#' UK census 2021
+#' 
+#' $R$  package that include data, boundaries and a Shiny app to visualize interactively the new preliminary results of the last 2021 UK Census.
+#' As of June 2022, data only include "population" by "sex" and "age" in five-years classes plus "households", 
+#' with the lowest geographical detail being "Local Authority Districts" in England and Wales only.
+#'  
+#' @docType package
+#' @name census_uk_2021
+NULL
---
+++ R/data.R
@@ -0,0 +1,46 @@
+#' @importFrom data.table data.table
+NULL
+
+#' lookups
+#'
+#' List of all Local Authority Districts in England and wales, with correspondent Region
+#' 
+#' @format A data.table with the following columns:
+#' \describe{
+#'   \item{\code{ LAD }}{ ONS code for Local Authority Districts }
+#'   \item{\code{ x_lon }}{ Longitude of the geometric centroid for Local Authority Districts }
+#'   \item{\code{ y_lat }}{ Latitude of the geometric centroid for Local Authority Districts }
+#'   \item{\code{ area }}{ Area for Local Authority Districts }
+#'   \item{\code{ LADn }}{ ONS description for Local Authority Districts}
+#'   \item{\code{ RGN }}{ ONS code for Region (England only) }
+#'   \item{\code{ RGNn  }}{ ONS description for Region (England only) }
+#'   \item{\code{ CTRY  }}{ ONS code for Country }
+#'   \item{\code{ CTRYn  }}{ ONS description for Country }
+#' }
+#'
+'lookups'
+
+
+#' @import sf
+NULL
+
+#' LAD 
+#'
+#' Geographic Boundaries in \code{sf} format for Local Authority Districts in England and Wales (last update: May 2021). 
+#'
+#' More information can be found in the table \code{lookups}.
+#'
+#' For more details, consult the website \url{https://geoportal.statistics.gov.uk/}
+#'
+'LAD'
+
+#' RGN 
+#'
+#' Geographic Boundaries in \code{sf} format for Regions in England (last update: May 2021). 
+#'
+#' More information can be found in the table \code{lookups}.
+#'
+#' For more details, consult the website \url{https://geoportal.statistics.gov.uk/}
+#'
+'RGN'
+
