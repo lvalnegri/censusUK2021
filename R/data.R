7da2ab2a34119c9c5c85d32c2dd40ca8e4be117e
datamaps <l.valnegri@datamaps.co.uk>
2022-06-27 15:16:26 +0100

    added UTLA

--- R/data.R
+++ R/data.R
@@ -12,11 +12,15 @@ NULL
 #'   \item{\code{ y_lat }}{ Latitude of the geometric centroid for Local Authority Districts }
 #'   \item{\code{ area }}{ Area for Local Authority Districts }
 #'   \item{\code{ LADn }}{ ONS description for Local Authority Districts}
+#'   \item{\code{ UTLA }}{ ONS code for Upper Tier Local Authority }
+#'   \item{\code{ UTLAn }}{ ONS description for Upper Tier Local Authority }
 #'   \item{\code{ RGN }}{ ONS code for Region (England only) }
 #'   \item{\code{ RGNn  }}{ ONS description for Region (England only) }
 #'   \item{\code{ CTRY  }}{ ONS code for Country }
 #'   \item{\code{ CTRYn  }}{ ONS description for Country }
 #' }
+#'
+#' For more details, consult the website \url{https://geoportal.statistics.gov.uk/}
 #'
 'lookups'
 
@@ -34,6 +38,16 @@ NULL
 #'
 'LAD'
 
+#' UTLA 
+#'
+#' Geographic Boundaries in \code{sf} format for Upper Tier Local Authority in England and Wales (last update: Dec 2021). 
+#'
+#' More information can be found in the table \code{lookups}.
+#'
+#' For more details, consult the website \url{https://geoportal.statistics.gov.uk/}
+#'
+'UTLA'
+
 #' RGN 
 #'
 #' Geographic Boundaries in \code{sf} format for Regions in England (last update: May 2021). 
@@ -43,4 +57,3 @@ NULL
 #' For more details, consult the website \url{https://geoportal.statistics.gov.uk/}
 #'
 'RGN'
-
