af6badd6bd2c931ba062e5b5d539bb1a0f39ed8a
datamaps <l.valnegri@datamaps.co.uk>
2022-06-27 15:16:03 +0100

    some corrections

--- R/censusUK2021.R
+++ R/censusUK2021.R
@@ -1,13 +1,16 @@
 #' UK Census 2021
 #' 
-#' $R$  package that include data, boundaries and a Shiny app to visualize interactively the new preliminary results of the last 2021 UK Census.
-#' As of June 2022, data only include "population" by "sex" and "age" in five-years classes plus "households", 
-#' with the lowest geographical detail being "Local Authority Districts" in England and Wales only.
+#' `R` package that include data, boundaries and a Shiny app to visualize interactively the new preliminary results of the last 2021 UK Census.
+#' 
+#' As of June 2022, data only include `population` by `sex` and `age` in five-years classes plus `households`, 
+#' with the lowest geographical detail being `Local Authority Districts` in England and Wales only.
 #'  
 #' Attributions:
-#' - Contains OS data © Crown copyright and database rights [2021]
-#' - Contains National Statistics data © Crown copyright and database rights [2021]
-#' - Source: Office for National Statistics licensed under the Open Government Licence v.3.0
+#' \describe{
+#'   \item{ Contains OS data © Crown copyright and database rights [2021] }
+#'   \item{ Contains National Statistics data © Crown copyright and database rights [2021] }
+#'   \item{ Source: Office for National Statistics licensed under the Open Government Licence v.3.0 }
+#' }
 #' 
 #' @docType package
 #' @name censusUK2021
